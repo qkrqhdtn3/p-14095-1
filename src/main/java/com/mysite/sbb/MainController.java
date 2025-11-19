1afdf53e9e1c7041f04f95cbac65af24458c7d89
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-11-19 15:28:02 +0900

    2-02 간단한 웹 프로그램 만들기

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,15 +2,20 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public void index(){
-        System.out.println("index 호출");
+    @ResponseBody
+    public String index(){
+//        System.out.println("index 호출");
+        return "index";
     }
     @GetMapping("/hello")
-    public void hello(){
-        System.out.println("hello 호출");
+    @ResponseBody
+    public String hello(){
+//        System.out.println("hello 호출");
+        return "hello";
     }
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -2,12 +2,15 @@ package com.mysite.sbb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.ActiveProfiles;
+@ActiveProfiles("test")
 @SpringBootTest
 class SbbApplicationTests {
 
+
 	@Test
 	void contextLoads() {
+
 	}
 
 }
